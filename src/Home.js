9c5b05a8bbfd7a99b4086f5fe93cb45c09bae80f
jefftechhub <jefftechhub@gmail.com>
2024-01-18 23:54:50 -0800

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { data } from "./data";
+import { FaCirclePlus, FaCircleMinus } from "react-icons/fa6";
 import "./Home.css";
 
 const Home = () => {
-  return <h1>Home Page</h1>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <section>
+      <h1>Questions </h1>
+      <div>
+        {questions.map((question) => {
+          return (
+            <QuestionTemplate
+              key={question.id}
+              {...question}
+            ></QuestionTemplate>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+const QuestionTemplate = ({ quiz, answer }) => {
+  const [showing, setShowing] = useState(false);
+
+  return (
+    <div className="quiz-section">
+      <div className="quiz">
+        <h2>{quiz} </h2>
+        <button
+          onClick={() => {
+            setShowing(!showing);
+          }}
+        >
+          {showing ? (
+            <FaCircleMinus className="btnShow"></FaCircleMinus>
+          ) : (
+            <FaCirclePlus className="btnShow"></FaCirclePlus>
+          )}
+        </button>
+      </div>
+      {showing && <p>{answer}</p>}
+    </div>
+  );
 };
 
 export default Home;
